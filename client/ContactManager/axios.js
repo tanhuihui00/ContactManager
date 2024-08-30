eb8f4cdccd7ac05b3f7b677927bfc1f22aec5245
tanhuihui <huihuitan6607@gmail.com>
2024-08-31 06:19:25 +0800

    update render url path

--- client/ContactManager/axios.js
+++ client/ContactManager/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://contactmanager1-t5jh.onrender.com/",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
